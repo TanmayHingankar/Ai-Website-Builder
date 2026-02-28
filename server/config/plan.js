a3a699fa7657ee5d843c815375ffb296e1bd337e
Tanmay Hingankar <tuhara-email@example.com>
2026-02-28 13:04:51 +0530

    Add PLANS config for billing

---
+++ server/config/plan.js
@@ -0,0 +1,17 @@
+export const PLANS = {
+  free: {
+    plan: "free",
+    price: 0,
+    credits: 100,
+  },
+  pro: {
+    plan: "pro",
+    price: 499,
+    credits: 500,
+  },
+  enterprise: {
+    plan: "enterprise",
+    price: 1499,
+    credits: 1000,
+  },
+};
